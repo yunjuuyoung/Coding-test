d9609ecdb946fb4dace2e4f248e40bc20558c0af
JuYoung <128597386+juyoung07@users.noreply.github.com>
2023-10-07 00:38:47 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] arg){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
