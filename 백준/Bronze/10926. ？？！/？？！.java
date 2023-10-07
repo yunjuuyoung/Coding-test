96a84d90999c6e1a4fa4c537aad9eafb189fb33d
JuYoung <128597386+juyoung07@users.noreply.github.com>
2023-10-08 00:25:33 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] arg){
+        
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        System.out.println( st + "??!" );
+    }
+}
