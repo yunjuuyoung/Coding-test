936f94398ad6983cc30d31e1c88bd5707cd67dcc
윤주영 <128597386+juyoung07@users.noreply.github.com>
2023-10-14 22:46:05 +0900

    [Bronze V] Title: A+B - 5, Time: 224 ms, Memory: 17892 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if(A!=0){
+                System.out.println((A+B));
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
