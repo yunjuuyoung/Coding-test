9c49afa531a7dadbd5047dd90bec60bec308c811
윤주영 <128597386+juyoung07@users.noreply.github.com>
2024-04-10 21:09:14 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -1,13 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for (int i = 0; i < myString.length(); i++) {
-            char c = myString.charAt(i);
-            if (Character.isLowerCase(c))
-                answer += Character.toUpperCase(c);
-            else
-                answer += c;
-        }
-        return answer;
+        return myString.toUpperCase();
     }
 }
