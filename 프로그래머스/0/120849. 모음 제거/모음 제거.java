205be239597f3cce0fa1e6bf47476fb97b30addb
윤주영 <128597386+juyoung07@users.noreply.github.com>
2024-04-25 15:54:55 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,18 +1,19 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for (int i = 0; i < my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            switch (c) {
-                case 'a': break;
-                case 'e': break;
-                case 'i': break;
-                case 'u': break;
-                case 'o': break;
-                default : 
-                    answer += c;
-            }
-        }
-        return answer;
+        // String answer = "";
+        // for (int i = 0; i < my_string.length(); i++) {
+        //     char c = my_string.charAt(i);
+        //     switch (c) {
+        //         case 'a': break;
+        //         case 'e': break;
+        //         case 'i': break;
+        //         case 'u': break;
+        //         case 'o': break;
+        //         default : 
+        //             answer += c;
+        //     }
+        // }
+        
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
