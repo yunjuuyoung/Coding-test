f6fd318a9b03e8e978d578674e5a38f061526dc4
윤주영 <128597386+juyoung07@users.noreply.github.com>
2024-05-15 22:41:50 +0900

    [level 0] Title: 암호 해독, Time: 13.31 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        for (int i = 0; i < cipher.length(); i++) {
+            char c = cipher.charAt(i);
+            
+            if ((i + 1) % code == 0) 
+                answer += c;
+        }
+        return answer;
+    }
+}
