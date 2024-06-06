9d68460765170fa140acdd3f492c8a99cc43f031
윤주영 <128597386+juyoung07@users.noreply.github.com>
2024-06-06 22:05:44 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String s = Integer.toString(order);
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == '3' || c == '6' || c == '9')
+                answer++;
+        }
+        
+        return answer;
+    }
+}
