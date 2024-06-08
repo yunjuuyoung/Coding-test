d5b37619116723bc5ccd2692fd67c418145982aa
윤주영 <128597386+juyoung07@users.noreply.github.com>
2024-06-08 18:39:57 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1];
+        int idx = 0;
+        
+        for (int i = 0; i < names.length; i += 5) {
+            answer[idx] = names[i];
+            idx++;
+        }
+        return answer;
+    }
+}
