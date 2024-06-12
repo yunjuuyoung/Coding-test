71bc9d87a37ca8aad37b78889a28357edadf9ad3
윤주영 <128597386+juyoung07@users.noreply.github.com>
2024-06-13 00:15:26 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.42 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n, String control) {
+        
+        for (int i = 0; i < control.length(); i++) {
+            char c = control.charAt(i);
+            
+            if (c == 'w')
+                n += 1;
+            else if (c == 's')
+                n -= 1;
+            else if (c == 'd')
+                n += 10;
+            else if (c == 'a')
+                n -= 10;
+        }
+        
+        return n;
+    }
+}
