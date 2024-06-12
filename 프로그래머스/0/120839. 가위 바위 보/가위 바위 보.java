babbc3ba21713b3b2ce77f4b73c55fe00435542c
윤주영 <128597386+juyoung07@users.noreply.github.com>
2024-06-12 23:49:49 +0900

    [level 0] Title: 가위 바위 보, Time: 2.42 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        for (int i = 0; i < rsp.length(); i++) {
+            char c = rsp.charAt(i);
+            
+            if (c == '2')
+                answer += "0";
+            else if (c == '5')
+                answer += "2";
+            else if (c == '0')
+                answer += "5";
+        }
+        
+        return answer;
+    }
+}
