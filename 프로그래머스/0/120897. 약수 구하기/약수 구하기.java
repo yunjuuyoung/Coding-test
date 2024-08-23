512d31bfd8c535d5ff8a7669508fc61b28e65e0c
윤주영 <128597386+juyoung07@users.noreply.github.com>
2024-08-23 23:35:48 +0900

    [level 0] Title: 약수 구하기, Time: 0.18 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                list.add(i);
+        }
+             
+        int answer[] = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i).intValue();
+        }
+             
+        return answer;
+    }
+}
