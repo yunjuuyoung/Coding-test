6931a4bec5a0e75739744b8913fc32dd0376be33
윤주영 <128597386+juyoung07@users.noreply.github.com>
2024-08-23 23:45:22 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.57 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -3,10 +3,7 @@ import java.util.*;
 class Solution {
     public String solution(String my_string) {
         char c[] = my_string.toLowerCase().toCharArray();
-        Arrays.sort(c);
-        
-        String answer = new String(c);
-        
-        return answer;
+        Arrays.sort(c);        
+        return new String(c);
     }
 }
