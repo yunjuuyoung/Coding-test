3f38d0393fe0fc01da13c98cd01838c1ec684110
윤주영 <128597386+juyoung07@users.noreply.github.com>
2025-01-07 00:03:47 +0900

    [level 0] Title: 커피 심부름, Time: 0.72 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -3,11 +3,10 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < order.length; i++) {
-            if (order[i].contains("americano"))
-                answer += 4500;
-            else if (order[i].contains("cafelatte"))
+            if (order[i].contains("cafelatte"))
                 answer += 5000;
-            else answer += 4500;
+            else
+                answer += 4500;
         }
         
         return answer;
