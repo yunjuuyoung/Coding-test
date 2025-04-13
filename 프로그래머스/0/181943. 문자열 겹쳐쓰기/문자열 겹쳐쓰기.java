a7f52ceabfc12d1dc24d9bac66a9dc23996dd1cb
Yun Juyoung <yunju5090@gmail.com>
2025-04-13 17:46:53 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 19.13 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (i >= s && i <= overwrite_string.length() + s - 1) {
+                answer += overwrite_string.charAt(i - s);
+            } else {
+                answer += my_string.charAt(i);
+            }
+        }
+        
+        return answer;
+    }
+}
