a10526411014ed0099b1704161fa307916a6983f
Yun Juyoung <yunju5090@gmail.com>
2025-05-12 22:31:39 +0900

    [level 0] Title: 배열 만들기 3, Time: 9.42 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < intervals.length; i++) {
+            for (int j = intervals[i][0]; j <= intervals[i][1]; j++) {
+                list.add(arr[j]);
+            }
+        }
+        
+        int answer[] = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
