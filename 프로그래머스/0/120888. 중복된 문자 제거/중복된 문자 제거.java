802d7a3e7dae42046810611949622db4f4ddfa6c
Yun Juyoung <yunju5090@gmail.com>
2025-05-13 21:28:01 +0900

    [level 0] Title: 중복된 문자 제거, Time: 14.45 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (!answer.contains(c + "")) {
+                answer += c;
+            }
+        }
+        
+        return answer;
+    }
+}
