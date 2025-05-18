3301b2235d36399e5dc6255f2b5f15cac1886c02
Yun Juyoung <yunju5090@gmail.com>
2025-05-18 22:46:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.21 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String str[] = my_string.split("[a-zA-Z]");
+        
+        for (int i = 0; i < str.length; i++) {
+            if (!str[i].equals("")) {
+                answer += Integer.parseInt(str[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
